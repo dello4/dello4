b3b5240200ec59e0439db7d3c4cab02d117c49b5
Andrea Dell'Orto <andrea.dellorto.it88@gmail.com>
2016-03-29 13:25:06 +0200

    backup

--- JIRAScriptExporter/JIRAScriptExporter/JIRA_reader.py
+++ JIRAScriptExporter/JIRAScriptExporter/JIRA_reader.py
@@ -3,15 +3,22 @@ Created on 25/mar/2016
 
 @author: Andrea Dell'Orto
 '''
+from JIRAScriptExporter import script_exporter
+from jira import JIRA
 
 class JIRAReader(object):
     '''
     Class who connects to JIRA and gest csv file
     '''
+    
+    conf = script_exporter.ConfigFileReader()
 
-
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+    def __init__(self, conf):
+        self.conf = conf
         
+    def connect(self):
+        if self.conf.has_section('jira_url'):
+            try:
+                jira = JIRA(self.conf.get('jira_url', 'url'))
+            except(ConnectionError, ConnectionRefusedError):
+                print('Error connecting ' + self.conf.get('jira_url', 'url'))
--- JIRAScriptExporter/JIRAScriptExporter/script_exporter.py
+++ JIRAScriptExporter/JIRAScriptExporter/script_exporter.py
@@ -4,5 +4,16 @@ Created on 25/mar/2016
 @author: Andrea Dell'Orto
 '''
 
+import configparser
+import inspect
+import os
+import JIRAScriptExporter.JIRA_reader
+
+def ConfigFileReader():
+    cfgFilePath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + '\config.ini'
+    config = configparser.ConfigParser()
+    config.readfp(open(cfgFilePath))
+    return config
+
 if __name__ == '__main__':
-    pass
+    jira_reader = JIRAScriptExporter.JIRA_reader()
