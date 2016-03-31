c39110536c365fbd0bce34e771e3a8119b63582a
Andrea Dell'Orto <andrea.dellorto.it88@gmail.com>
2016-03-31 10:17:11 +0200

    Module and methods rename

--- ScriptValidatorUtils/script_validator.py
+++ ScriptValidatorUtils/script_validator.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 
-import os, re
+import os
 import configparser
 import inspect
 
-def quotesRemover(quotedString):
- stringWithoutQuotes = quotedString.replace('"', '')
- return stringWithoutQuotes
+def quotes_remover(quotedString):
+    stringWithoutQuotes = quotedString.replace('"', '')
+    return stringWithoutQuotes
  
 #Input directory for the script is read from file
 cfgFilePath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + '\JIRAExporter.cfg'
@@ -27,18 +27,18 @@ if config.has_section('dir'):
             scriptAsString = script.read().lower()
             logname = 'spool &path.&nomeschema._'+os.path.splitext(os.path.basename(script.name))[0]+'.log\n'
             
-			#Loading base elements from the configuration file
+            #Loading base elements from the configuration file
             strings = [logname.lower()]
             for (each_key, each_val) in config.items('base_elements'):
-                strings.append(quotesRemover(each_val))
+                strings.append(quotes_remover(each_val))
             if isDebug: print(strings)
-			#Errors list loaded from config file
+            #Errors list loaded from config file
             errorsList = []
             for (each_key, each_val) in config.items('errors'):
-                errorsList.append(quotesRemover(each_val))
+                errorsList.append(quotes_remover(each_val))
             if isDebug: print(errorsList)
-			#Verify the script
-			#Lowercase conversion of the entire script to perform statement analisys
+            #Verify the script
+            #Lowercase conversion of the entire script to perform statement analisys
             if all(s.lower() in scriptAsString for s in strings) and not any(s.lower() in scriptAsString for s in errorsList):
                 print("Script " + os.path.basename(script.name) + " è verificato.")
             else:
