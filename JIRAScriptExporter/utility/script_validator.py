d7e40414be3e165ac118fdffbe68669dfc775948
Andrea Dell'Orto <andrea.dellorto.it88@gmail.com>
2016-03-31 10:30:39 +0200

    New package

---
+++ JIRAScriptExporter/utility/script_validator.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+
+import os
+import configparser
+import inspect
+
+def quotes_remover(quotedString):
+    stringWithoutQuotes = quotedString.replace('"', '')
+    return stringWithoutQuotes
+ 
+#Input directory for the script is read from file
+cfgFilePath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + '\JIRAExporter.cfg'
+config = configparser.ConfigParser()
+config.readfp(open(cfgFilePath))
+
+isDebug = config.getboolean('sys','isDebug')
+isError = False
+
+if config.has_section('dir'):
+    inputdir = config.get('dir','input')
+
+    for file in os.listdir(inputdir):
+        #Only sql files
+        if file.endswith(".sql"):
+            file = os.path.join(inputdir,file)
+            script = open(file)
+            scriptAsString = script.read().lower()
+            logname = 'spool &path.&nomeschema._'+os.path.splitext(os.path.basename(script.name))[0]+'.log\n'
+            
+            #Loading base elements from the configuration file
+            strings = [logname.lower()]
+            for (each_key, each_val) in config.items('base_elements'):
+                strings.append(quotes_remover(each_val))
+            if isDebug: print(strings)
+            #Errors list loaded from config file
+            errorsList = []
+            for (each_key, each_val) in config.items('errors'):
+                errorsList.append(quotes_remover(each_val))
+            if isDebug: print(errorsList)
+            #Verify the script
+            #Lowercase conversion of the entire script to perform statement analisys
+            if all(s.lower() in scriptAsString for s in strings) and not any(s.lower() in scriptAsString for s in errorsList):
+                print("Script " + os.path.basename(script.name) + " è verificato.")
+            else:
+                print("ERR02: Lo script " + file + " non rispetta lo standard!! - Non è presente o è scritto male ")
+                script.close()
+                isError = True
+                break
+            script.close()
+    if not isError:
+        print("Tutti gli script sembrano essere coerenti!")
+else:
+    print("ERR01 - File di configurazione non trovato.")
+
+input("Press any key to exit...")
